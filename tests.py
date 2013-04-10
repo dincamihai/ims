89b8289f77f3c2508832192c7805f84bdc72fdf3
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-04-10 21:04:43 +0300

    add tests case

---
+++ tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import unittest
+
+class IMSTestCase(unittest.TestCase):
+
+    def test_counter_reset(self):
+        assert False
+
+if __name__=="__main__":
+    unittest.main()
