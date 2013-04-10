5cabd1af0a0944caebe85c28848c4eda8c018ac4
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-04-10 22:25:15 +0300

    refactoring

--- listener/tests/test_ims.py
+++ listener/tests/test_ims.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 import unittest
+from listener import listener
 
 class IMSTestCase(unittest.TestCase):
 
     def test_counter_reset(self):
-        assert False
+        counter = {1: 1, 2: 0}
+        listener.reseter(1)
+        import pytest; pytest.set_trace()
 
 if __name__=="__main__":
     unittest.main()
